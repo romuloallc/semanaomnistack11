23f0a9f44e7cb6705cb6ff3bdcf48c18596a84d9
romuloallc <romulo.allc@gmail.com>
2020-03-26 15:19:58 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-    return ( < a div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Salve Salve <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> < /
-        header > <
-        /div>
+import './global.css';
+
+import Routes from './routes';
+
+function App() {    
+    return (
+        <Routes />
     );
 }
 
 export default App;
-a
